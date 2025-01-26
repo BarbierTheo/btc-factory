4ee58820e2916c3aa7106dc21b844e0a453339c1
BarbierTheo <lebarbier.lh@gmail.com>
2025-01-27 00:56:46 +0100

    Ajout du système de quotes

---
+++ asset/js/quotes.js
@@ -0,0 +1,25 @@
+function getRandomNumber() {
+    return Math.floor(Math.random() * 20) + 1;
+}
+
+function randomQuotes() {
+    randomNumber = getRandomNumber()
+    fetch("/asset/js/json/quotes.json")
+    .then((response) => response.json())
+    .then((data) => {
+        document.getElementById('quotes').innerHTML += `
+                        <div class="p-2"> 
+                            <p class="text-lg font-semibold">${data[randomNumber].title}</p>
+                            <p>${data[randomNumber].text}</p>
+                            <hr class="my-2 bg-slate-600">
+                        </div>`
+    })
+
+}
+
+
+randomQuotes()
+    
+setInterval(function () {
+    randomQuotes()
+}, 45000)
