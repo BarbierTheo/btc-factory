d1960d2ff4e986e84304c18da65af9d373d98499
BarbierTheo <lebarbier.lh@gmail.com>
2025-01-27 18:50:18 +0100

    miner ne va plus en négatif, condition job

--- asset/js/jobs.js
+++ asset/js/jobs.js
@@ -1,34 +1,64 @@
 
 let job = 1
 
-// MINER
+function showGame() {
 
-let bitcoinclick = 0
-let stillMiner = 5000
-let minerGen = 0
+    // MINER
 
-document.getElementById('jobInterface').innerHTML = `
+    if (job == 1) {
+
+        let bonusMiner = 1
+        let btcClicMiner = 0
+        let mining = 0.0002 * bonusMiner
+        let baseMiner = round(1 / mining)
+        let btcGenerate = 0
+        let stillMiner = round(baseMiner - btcClicMiner)
+
+        document.getElementById('jobInterface').innerHTML = `
                     <div class="flex flex-col justify-center self-center h-full gap-8 text-center">
                         <p class="text-3xl font-bold self-center">FREE BITCOIN</p>
                         <button class="self-center cursor-pointer" id="miner"><img src="./asset/img/icons/Warning.png"
                         alt="" class="w-[8rem]"></button>
                         <p>clics : <span id="clicksMiner">0</span></p>
-                        <p><span id="minerGen">0</span> bitcoins générés</p>
-                        <p>encore <span id="stillMiner">10000</span> clics pour un bitcoin complet !</p>
+                        <p><span id="btcGenerate">0</span> bitcoins générés</p>
+                        <p>encore <span id="stillMiner">???</span> clics pour un bitcoin complet !</p>
                     </div>  `
 
-let coinMiner = document.getElementById('miner')
-document.getElementById('stillMiner').innerHTML = '5000'
+        let coinMiner = document.getElementById('miner')
+        document.getElementById('stillMiner').innerHTML = '5000'
 
-coinMiner.addEventListener('click', () => {
+        coinMiner.addEventListener('click', () => {
 
-    bitcoin = round(bitcoin) + (0.0002)
-    minerGen = minerGen + (0.0002)
-    bitcoinclick++
-    document.getElementById('clicksMiner').innerHTML = bitcoinclick
-    document.getElementById('stillMiner').innerHTML = stillMiner - bitcoinclick
-    document.getElementById('minerGen').innerHTML = round(minerGen)
-    refreshdom()
-})
+            btcClicMiner++
+            stillMiner = (baseMiner - btcClicMiner)
+            bitcoin = round(bitcoin) + mining
+            btcGenerate = btcGenerate + (0.0002)
+            document.getElementById('clicksMiner').innerHTML = btcClicMiner
+
+            if (stillMiner == 1) {
+                baseMiner = baseMiner + round(1 / mining)
+                stillMiner = round(baseMiner - btcClicMiner)
+                document.getElementById('stillMiner').innerHTML = (stillMiner)
+                console.log(stillMiner)
+            } else {
+                document.getElementById('stillMiner').innerHTML = (stillMiner)
+            }
+
+            document.getElementById('btcGenerate').innerHTML = round(btcGenerate)
+
+            refreshdom()
+
+        })
+
+    }
+
+    // JEU 2
+
+    if (job == 2) {
+        console.log('jeu 2')
+    }
 
 
+}
+
+showGame()
